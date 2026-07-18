126a1d056bcce0d7a7c7e5668ce312e4bf99c941
Mahasree <Mahasree.8c.vhs@gmail.com>
2026-07-18 12:44:07 +0530

    randon user commit

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,68 @@
 // alert("hello from js")
 console.log("hello from js")
+data = {
+    "name":" Maha",
+    "gender": "Female",
+}
+//data.name// Maha
+var users = [
+    {
+        "name" : "Vaishno",
+        "gender" : "Male",
+        "img" : "john.png"
+    },
+    {
+        "name" : "Maha",
+        "gender" : "Female",
+        "img" : "jane.png"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
+    // get the elements
+
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
+}
+function randomUser(){
+    fetch('https://randomuser.me/api/')
+        .then(function(response){
+            // convert raw text to json
+            return response.json();
+        })
+        // dom mannipulation with API data
+        .then(function(data){
+
+            var userData = data.results[0];
+
+            // 1: image
+            var userImage = document.getElementById("user-image");
+            userImage.src = userData.picture.large;
+
+            // 2: name
+            var userName = document.getElementById("user-name");
+            userName.innerHTML = userData.name.first + " " +
+                                    userData.name.last;
+
+            // 3: gender
+            var userGender = document.getElementById("user-gender");
+            userGender.innerHTML = userData.gender;
+        })
+        // handle error
+        .catch(function(err){
+            console.log(err);
+        })
+}
